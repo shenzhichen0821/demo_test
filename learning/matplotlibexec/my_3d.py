90f70f2410ac1138d0d90fc8157d16fe8a4f81dc
zhichen.szc <zhichen.szc@antfin.com>
2018-08-30 21:20:05 +0800

    modify 3d

--- learning/matplotlibexec/my_3d.py
+++ learning/matplotlibexec/my_3d.py
@@ -3,14 +3,14 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
 ax = Axes3D(plt.figure())
-x = np.arange(1, 100, 1)
-y = np.arange(1, 100, 1)
-x, y = np.meshgrid(x, y)
-z = np.array(100/x + x/y + y)
 
-ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap=plt.get_cmap('rainbow'))
-plt.xlabel('x axis')
-plt.ylabel('y axis')
-ax.set_zlabel('z axis')
+x = np.arange(-4, 4, 0.25)
+y = np.arange(-4, 4, 0.25)
+x, y = np.meshgrid(x, y)
+z = np.sin(np.sqrt(x**2 + y**2))
+
+ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap=plt.get_cmap('rainbow'), edgecolor='k')
+ax.contourf(x, y, z, zdir='z', offset=-2, cmap='rainbow')
+ax.set_zlim(-2, 2)
 
 plt.show()
